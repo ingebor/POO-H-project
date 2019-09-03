4d12bb89cd6490daca44862ce345e49ea6407f91
AaronPiv <AaronPiv@LAPTOP-KT2IHOTL>
2019-09-03 15:43:55 -0600

    Declare las varibales para los ArraysList de enfermedades y medicinas. Ademas cree ambas listas. Tambien empece a programar los metodos agragarMedicina, agregarEnfermedades y actualizarEnfermedades.

--- ProyectoPOO/src/CatalogoSalud.java
+++ ProyectoPOO/src/CatalogoSalud.java
@@ -1,6 +1,50 @@
-
+import java.util.ArrayList;
 public class CatalogoSalud {
-	private String MePuedenVer;
-	private String soyAyleen;
-	private String SoySayA;
+	private ArrayList<Enfermedad>enfermedades;
+	private ArrayList<Medicina>medicinas;
+	
+	/**
+	 * @param Constructor de la clase CatalogoSalud
+	 */
+	public CatalogoSalud()
+	{
+		enfermedades = new ArrayList<Enfermedad>();
+		medicinas = new ArrayList<Medicina>();
+	}
+	
+	
+	/**
+	 * @param Agrega una medicina en la lista del array de medicinas 
+	 * 
+	 */
+	public void agregarMedicina(Medicina nuevaMedicina, int i)
+	{	
+		medicinas.add(i, nuevaMedicina);	
+	}
+	
+	/**
+	 * @param Agrega una enfermedad en la lista del array de enfermedades 
+	 * 
+	 */
+	public void agregarEnfermedad(Enfermedad nuevaEnfermedad, int i)
+	{
+		enfermedades.add(i,nuevaEnfermedad);
+	}
+	
+	
+	/**
+	 * @param Actualiza una enfermedad, para agregar mas informacion, nuevos sintomas o el nombre de la enen la lista del array de enfermedades 
+	 * 
+	 */
+	public void ActualizarEnfermedad(Enfermedad enfermedadActualizado, int i) {
+		if(i<=enfermedades.size() && enfermedades.get(i) != null)
+			enfermedades.add(i, enfermedadActualizado);
+	}
+	
+
+	 
+	
+	
+	
+		
 }
