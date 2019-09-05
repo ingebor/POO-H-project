915788883d604596550ec0274babad5ebb292c3a
rudiks <rudiks@192.168.1.17>
2019-09-04 21:44:15 -0600

    GUI del Paciente

--- ProyectoPOO/src/MenuPaciente.java
+++ ProyectoPOO/src/MenuPaciente.java
@@ -1,6 +1,8 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 
 public class MenuPaciente {
 
@@ -36,6 +38,16 @@ public class MenuPaciente {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 450, Short.MAX_VALUE)
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 278, Short.MAX_VALUE)
+		);
+		frame.getContentPane().setLayout(groupLayout);
 	}
 
 }
