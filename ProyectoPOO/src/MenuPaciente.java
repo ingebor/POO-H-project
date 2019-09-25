f8bc270a80b67553e4dd557c7d58cb9b22c99d01
Julio Monzón <mon19226@uvg.edu.gt>
2019-09-25 15:26:51 -0600

    Actalizaciones en MenuPaciente y MenuDoctor
    A ambas GUI les agruegue un poco de código que hace que la interfaz se
    vea más amigable y no tan rígida, está en el main de cada una de las
    interfaces. En la GUI del paciente coloqué todo lo que hizo Aarón dentro
    de un panel de ingreso de datos para que se vea más ordenado

--- ProyectoPOO/src/MenuPaciente.java
+++ ProyectoPOO/src/MenuPaciente.java
@@ -14,10 +14,14 @@ import javax.swing.SwingConstants;
 import javax.swing.JComboBox;
 import java.awt.Font;
 import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+import javax.swing.UIManager;
+import java.awt.Color;
 
 public class MenuPaciente extends JFrame {
 
 	private JPanel contentPane;
+	private JPanel panelIngreso;
 	private JButton btnIngresoSintomas;
 	private JButton btnBuscarSintomas;
 	private JButton btnBuscarMedicina;
@@ -38,8 +42,25 @@ public class MenuPaciente extends JFrame {
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
+			
 			public void run() {
 				try {
+					try {
+			            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+			                if ("Nimbus".equals(info.getName())) {
+			                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+			                    break;
+			                }
+			            }
+			        } catch (ClassNotFoundException ex) {
+			            java.util.logging.Logger.getLogger(MenuPaciente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+			        } catch (InstantiationException ex) {
+			            java.util.logging.Logger.getLogger(MenuPaciente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+			        } catch (IllegalAccessException ex) {
+			            java.util.logging.Logger.getLogger(MenuPaciente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+			        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+			            java.util.logging.Logger.getLogger(MenuPaciente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+			        }
 					MenuPaciente frame = new MenuPaciente();
 					frame.setVisible(true);
 				} catch (Exception e) {
@@ -55,118 +76,124 @@ public class MenuPaciente extends JFrame {
 	public MenuPaciente() {
 		getContentPane().setLayout(null);
 		
-		JLabel lbldolorCabeza = new JLabel("Dolor de Cabeza:");
-		lbldolorCabeza.setBounds(10, 47, 83, 14);
-		getContentPane().add(lbldolorCabeza);
+		panelIngreso = new JPanel();
+		panelIngreso.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Busqueda de enfermedad por sintomas", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panelIngreso.setBounds(10, 11, 517, 358);
+		getContentPane().add(panelIngreso);
+		panelIngreso.setLayout(null);
 		
-		JComboBox comboBoxDolor_de_cabeza = new JComboBox();
-		comboBoxDolor_de_cabeza.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
-		comboBoxDolor_de_cabeza.setBounds(113, 44, 41, 20);
-		getContentPane().add(comboBoxDolor_de_cabeza);
+		JLabel lblIntruccion = new JLabel("Indique qu\u00E9 s\u00EDntomas padece:");
+		lblIntruccion.setBounds(10, 21, 221, 19);
+		panelIngreso.add(lblIntruccion);
+		lblIntruccion.setFont(new Font("Tahoma", Font.BOLD, 15));
+		
+		JLabel lbldolorCabeza = new JLabel("Dolor de Cabeza:");
+		lbldolorCabeza.setBounds(10, 51, 142, 14);
+		panelIngreso.add(lbldolorCabeza);
 		
 		JLabel lbldolorEstomago = new JLabel("Dolor de Estomago:");
-		lbldolorEstomago.setBounds(10, 72, 94, 14);
-		getContentPane().add(lbldolorEstomago);
-		
-		JComboBox comboBoxDolorEstomago = new JComboBox();
-		comboBoxDolorEstomago.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
-		comboBoxDolorEstomago.setBounds(113, 69, 41, 20);
-		getContentPane().add(comboBoxDolorEstomago);
+		lbldolorEstomago.setBounds(10, 76, 142, 14);
+		panelIngreso.add(lbldolorEstomago);
 		
 		JLabel lblvomito = new JLabel("Vomitos:");
-		lblvomito.setBounds(10, 97, 41, 14);
-		lblvomito.setHorizontalAlignment(SwingConstants.TRAILING);
-		getContentPane().add(lblvomito);
-		
-		JComboBox comboBoxVomito = new JComboBox();
-		comboBoxVomito.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
-		comboBoxVomito.setBounds(113, 94, 41, 20);
-		getContentPane().add(comboBoxVomito);
+		lblvomito.setBounds(10, 101, 142, 14);
+		panelIngreso.add(lblvomito);
+		lblvomito.setHorizontalAlignment(SwingConstants.LEFT);
 		
 		JLabel lbldiarrea = new JLabel("Diarrea:");
-		lbldiarrea.setBounds(10, 122, 39, 14);
-		lbldiarrea.setHorizontalAlignment(SwingConstants.TRAILING);
-		getContentPane().add(lbldiarrea);
-		
-		JComboBox comboBoxDiarrea = new JComboBox();
-		comboBoxDiarrea.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
-		comboBoxDiarrea.setBounds(113, 119, 41, 20);
-		getContentPane().add(comboBoxDiarrea);
+		lbldiarrea.setBounds(10, 126, 142, 14);
+		panelIngreso.add(lbldiarrea);
+		lbldiarrea.setHorizontalAlignment(SwingConstants.LEFT);
 		
 		JLabel lblestornudo = new JLabel("Estornudo:");
-		lblestornudo.setBounds(10, 147, 53, 14);
-		getContentPane().add(lblestornudo);
-		
-		JComboBox comboBoxEstornudo = new JComboBox();
-		comboBoxEstornudo.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
-		comboBoxEstornudo.setBounds(113, 144, 41, 20);
-		getContentPane().add(comboBoxEstornudo);
+		lblestornudo.setBounds(10, 151, 142, 14);
+		panelIngreso.add(lblestornudo);
 		
 		JLabel lbltos = new JLabel("Tos:");
-		lbltos.setBounds(10, 172, 21, 14);
-		getContentPane().add(lbltos);
-		
-		JComboBox comboBoxTos = new JComboBox();
-		comboBoxTos.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
-		comboBoxTos.setBounds(113, 169, 41, 20);
-		getContentPane().add(comboBoxTos);
+		lbltos.setBounds(10, 176, 142, 14);
+		panelIngreso.add(lbltos);
 		
 		JLabel lbldolorGeneral = new JLabel("Dolor General:");
-		lbldolorGeneral.setBounds(10, 197, 69, 14);
-		getContentPane().add(lbldolorGeneral);
-		
-		JComboBox comboBoxDolorGeneral = new JComboBox();
-		comboBoxDolorGeneral.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
-		comboBoxDolorGeneral.setBounds(113, 194, 41, 20);
-		getContentPane().add(comboBoxDolorGeneral);
+		lbldolorGeneral.setBounds(10, 201, 142, 14);
+		panelIngreso.add(lbldolorGeneral);
 		
 		JLabel lblfaltaEnergia = new JLabel("Falta de energia:");
-		lblfaltaEnergia.setBounds(10, 223, 82, 14);
-		getContentPane().add(lblfaltaEnergia);
+		lblfaltaEnergia.setBounds(11, 226, 141, 14);
+		panelIngreso.add(lblfaltaEnergia);
 		
-		JComboBox comboBoxFaltaEnergia = new JComboBox();
+		comboBoxDolor_de_cabeza = new JComboBox();
+		comboBoxDolor_de_cabeza.setBounds(148, 51, 55, 20);
+		panelIngreso.add(comboBoxDolor_de_cabeza);
+		comboBoxDolor_de_cabeza.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
+		
+		comboBoxDolorEstomago = new JComboBox();
+		comboBoxDolorEstomago.setBounds(148, 73, 55, 20);
+		panelIngreso.add(comboBoxDolorEstomago);
+		comboBoxDolorEstomago.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
+		
+		comboBoxVomito = new JComboBox();
+		comboBoxVomito.setBounds(148, 98, 55, 20);
+		panelIngreso.add(comboBoxVomito);
+		comboBoxVomito.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
+		
+		comboBoxDiarrea = new JComboBox();
+		comboBoxDiarrea.setBounds(148, 123, 55, 20);
+		panelIngreso.add(comboBoxDiarrea);
+		comboBoxDiarrea.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
+		
+		comboBoxEstornudo = new JComboBox();
+		comboBoxEstornudo.setBounds(148, 148, 55, 20);
+		panelIngreso.add(comboBoxEstornudo);
+		comboBoxEstornudo.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
+		
+		comboBoxTos = new JComboBox();
+		comboBoxTos.setBounds(148, 173, 55, 20);
+		panelIngreso.add(comboBoxTos);
+		comboBoxTos.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
+		
+		comboBoxDolorGeneral = new JComboBox();
+		comboBoxDolorGeneral.setBounds(148, 198, 55, 20);
+		panelIngreso.add(comboBoxDolorGeneral);
+		comboBoxDolorGeneral.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
+		
+		comboBoxFaltaEnergia = new JComboBox();
+		comboBoxFaltaEnergia.setBounds(148, 223, 55, 20);
+		panelIngreso.add(comboBoxFaltaEnergia);
 		comboBoxFaltaEnergia.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
-		comboBoxFaltaEnergia.setBounds(113, 220, 41, 20);
-		getContentPane().add(comboBoxFaltaEnergia);
 		
-		JButton btnIngresoSintomas = new JButton("Ingresar Sintomas");
-		btnIngresoSintomas.setBounds(10, 257, 119, 23);
-		getContentPane().add(btnIngresoSintomas);
-		
-		JButton btnBuscarSintomas = new JButton("Buscar Sintomas");
-		btnBuscarSintomas.setBounds(346, 181, 111, 23);
-		getContentPane().add(btnBuscarSintomas);
-		
-		JButton btnBuscarMedicina = new JButton("Buscar Medicinas");
-		btnBuscarMedicina.setBounds(344, 338, 113, 23);
-		getContentPane().add(btnBuscarMedicina);
-		
-		JButton btnVolver = new JButton("Volver");
-		btnVolver.setBounds(394, 9, 63, 23);
-		getContentPane().add(btnVolver);
-		
-		JLabel lblIntruccion = new JLabel("Ingrese los sintomas que padece:");
-		lblIntruccion.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblIntruccion.setBounds(10, 0, 255, 37);
-		getContentPane().add(lblIntruccion);
+		btnIngresoSintomas = new JButton("Ingresar Sintomas");
+		btnIngresoSintomas.setBounds(10, 251, 165, 23);
+		panelIngreso.add(btnIngresoSintomas);
 		
 		txtBuscarSintomas = new JTextField();
-		txtBuscarSintomas.setBounds(222, 60, 235, 114);
-		getContentPane().add(txtBuscarSintomas);
+		txtBuscarSintomas.setBounds(247, 65, 235, 102);
+		panelIngreso.add(txtBuscarSintomas);
 		txtBuscarSintomas.setColumns(10);
 		
-		JLabel lblSintomasText = new JLabel("Sintomas:");
-		lblSintomasText.setBounds(222, 42, 69, 14);
-		getContentPane().add(lblSintomasText);
+		JLabel lblSintomasText = new JLabel("Sintomas");
+		lblSintomasText.setBounds(247, 51, 235, 14);
+		panelIngreso.add(lblSintomasText);
 		
 		txtMedicamentos = new JTextField();
-		txtMedicamentos.setBounds(222, 243, 235, 84);
-		getContentPane().add(txtMedicamentos);
+		txtMedicamentos.setBounds(247, 234, 235, 84);
+		panelIngreso.add(txtMedicamentos);
 		txtMedicamentos.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("Medicamentos necesarios:");
-		lblNewLabel.setBounds(222, 223, 148, 14);
-		getContentPane().add(lblNewLabel);
+		lblNewLabel.setBounds(249, 201, 148, 14);
+		panelIngreso.add(lblNewLabel);
+		
+		btnBuscarSintomas = new JButton("Buscar Sintomas");
+		btnBuscarSintomas.setBounds(328, 172, 148, 23);
+		panelIngreso.add(btnBuscarSintomas);
+		
+		btnBuscarMedicina = new JButton("Buscar Medicinas");
+		btnBuscarMedicina.setBounds(328, 324, 150, 23);
+		panelIngreso.add(btnBuscarMedicina);
+		
+		btnVolver = new JButton("Volver a Inicio");
+		btnVolver.setBounds(311, 21, 171, 23);
+		panelIngreso.add(btnVolver);
 		
 		
 
