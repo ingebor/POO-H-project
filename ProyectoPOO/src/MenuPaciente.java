46327f082d9ca51fe6a1c89539b61f0722667023
AaronPiv <AaronPiv@LAPTOP-KT2IHOTL>
2019-09-25 21:25:32 -0600

    Ya termine la Gui con su metodos implementados :)

--- ProyectoPOO/src/MenuPaciente.java
+++ ProyectoPOO/src/MenuPaciente.java
@@ -1,11 +1,14 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import javax.swing.JOptionPane;
+
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.DefaultComboBoxModel;
@@ -28,7 +31,7 @@ public class MenuPaciente extends JFrame {
 	private JButton btnBuscarMedicina;
 	private JButton btnVolver;
 	private CatalogoSalud catalogo = new CatalogoSalud();
-	private JTextField txtBuscarSintomas;
+	private JTextField txtBuscarEnfermedad;
 	private JTextField txtMedicamentos;
 	private JComboBox comboBoxDolor_de_cabeza;
 	private JComboBox comboBoxDolorEstomago;
@@ -38,6 +41,10 @@ public class MenuPaciente extends JFrame {
 	private JComboBox comboBoxTos;
 	private JComboBox comboBoxDolorGeneral;
 	private JComboBox comboBoxFaltaEnergia;
+	private boolean dolorCabeza, dolorEstomago, vomito, diarrea, estornudo, tos, dolorGeneral, faltaEnergia;
+	private JTextField txtResultado;
+	private JTextField txtLaEnfermedad;
+	private JTextField textLaMedicina;
 	/**
 	 * Launch the application.
 	 */
@@ -76,10 +83,10 @@ public class MenuPaciente extends JFrame {
 	 */
 	public MenuPaciente() {
 		getContentPane().setLayout(null);
-		
+
 		panelIngreso = new JPanel();
 		panelIngreso.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Busqueda de enfermedad por sintomas", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		panelIngreso.setBounds(10, 11, 517, 358);
+		panelIngreso.setBounds(10, 11, 542, 367);
 		getContentPane().add(panelIngreso);
 		panelIngreso.setLayout(null);
 		
@@ -122,98 +129,229 @@ public class MenuPaciente extends JFrame {
 		lblfaltaEnergia.setBounds(11, 226, 141, 14);
 		panelIngreso.add(lblfaltaEnergia);
 		
+	
+		
 		comboBoxDolor_de_cabeza = new JComboBox();
 		comboBoxDolor_de_cabeza.setBounds(148, 51, 55, 20);
 		panelIngreso.add(comboBoxDolor_de_cabeza);
 		comboBoxDolor_de_cabeza.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
+		
+		if(comboBoxDolor_de_cabeza.equals("Si"))
+		{
+			dolorCabeza = true;
+		}
+		if(comboBoxDolor_de_cabeza.equals("No"))
+		{
+			dolorCabeza = false;
+		}
 		
 		comboBoxDolorEstomago = new JComboBox();
 		comboBoxDolorEstomago.setBounds(148, 73, 55, 20);
 		panelIngreso.add(comboBoxDolorEstomago);
 		comboBoxDolorEstomago.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
 		
+		if(comboBoxDolorEstomago.equals("Si"))
+		{
+			dolorEstomago = true;
+		}
+		if(comboBoxDolorEstomago.equals("No"))
+		{
+			dolorEstomago = false;
+		}
+		
+		
 		comboBoxVomito = new JComboBox();
 		comboBoxVomito.setBounds(148, 98, 55, 20);
 		panelIngreso.add(comboBoxVomito);
 		comboBoxVomito.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
+
+		if(comboBoxVomito.equals("Si"))
+		{
+			vomito = true;
+		}
+		if(comboBoxVomito.equals("No"))
+		{
+			vomito = false;
+		}
 		
 		comboBoxDiarrea = new JComboBox();
 		comboBoxDiarrea.setBounds(148, 123, 55, 20);
 		panelIngreso.add(comboBoxDiarrea);
 		comboBoxDiarrea.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
+
+		if(comboBoxDiarrea.equals("Si"))
+		{
+			diarrea = true;
+		}
+		if(comboBoxVomito.equals("No"))
+		{
+			diarrea = false;
+		}
 		
 		comboBoxEstornudo = new JComboBox();
 		comboBoxEstornudo.setBounds(148, 148, 55, 20);
 		panelIngreso.add(comboBoxEstornudo);
 		comboBoxEstornudo.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
 		
+		if(comboBoxEstornudo.equals("Si"))
+		{
+			estornudo = true;
+		}
+		if(comboBoxEstornudo.equals("No"))
+		{
+			estornudo = false;
+		}
+		
 		comboBoxTos = new JComboBox();
 		comboBoxTos.setBounds(148, 173, 55, 20);
 		panelIngreso.add(comboBoxTos);
 		comboBoxTos.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
+		
+		if(comboBoxTos.equals("Si"))
+		{
+			tos = true;
+		}
+		if(comboBoxTos.equals("No"))
+		{
+			tos = false;
+		}
 		
 		comboBoxDolorGeneral = new JComboBox();
 		comboBoxDolorGeneral.setBounds(148, 198, 55, 20);
 		panelIngreso.add(comboBoxDolorGeneral);
 		comboBoxDolorGeneral.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
 		
+		if(comboBoxDolorGeneral.equals("Si"))
+		{
+			dolorGeneral = true;
+		}
+		if(comboBoxDolorGeneral.equals("No"))
+		{
+			dolorGeneral = false;
+		}
+		
 		comboBoxFaltaEnergia = new JComboBox();
 		comboBoxFaltaEnergia.setBounds(148, 223, 55, 20);
 		panelIngreso.add(comboBoxFaltaEnergia);
 		comboBoxFaltaEnergia.setModel(new DefaultComboBoxModel(new String[] {"Si", "No"}));
 		
-		btnIngresoSintomas = new JButton("Ingresar Sintomas");
-		btnIngresoSintomas.setBounds(10, 251, 165, 23);
-		panelIngreso.add(btnIngresoSintomas);
+		if(comboBoxFaltaEnergia.equals("Si"))
+		{
+			faltaEnergia = true;
+		}
+		if(comboBoxFaltaEnergia.equals("No"))
+		{
+			faltaEnergia = false;
+		}
 		
-		txtBuscarSintomas = new JTextField();
-		txtBuscarSintomas.setBounds(247, 65, 235, 102);
-		panelIngreso.add(txtBuscarSintomas);
-		txtBuscarSintomas.setColumns(10);
+		txtBuscarEnfermedad = new JTextField();
+		txtBuscarEnfermedad.setBounds(247, 76, 235, 102);
+		panelIngreso.add(txtBuscarEnfermedad);
+		txtBuscarEnfermedad.setColumns(10);
 		
-		JLabel lblBuscarEnfermedad = new JLabel("Ingrese el nombre de la Enfermedad:");
-		lblBuscarEnfermedad.setBounds(247, 51, 235, 14);
+		JLabel lblBuscarEnfermedad = new JLabel("Nombre de la Enfermedad:");
+		lblBuscarEnfermedad.setBounds(213, 51, 130, 14);
 		panelIngreso.add(lblBuscarEnfermedad);
 		
 		txtMedicamentos = new JTextField();
-		txtMedicamentos.setBounds(247, 234, 235, 84);
+		txtMedicamentos.setBounds(247, 243, 235, 84);
 		panelIngreso.add(txtMedicamentos);
 		txtMedicamentos.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("Medicamentos necesarios:");
-		lblNewLabel.setBounds(247, 209, 148, 14);
+		lblNewLabel.setBounds(213, 218, 148, 14);
 		panelIngreso.add(lblNewLabel);
 		
+		milistener oyente = new milistener();
+		
 		btnBuscarEnfermedad = new JButton("Buscar Enfermedad");
-		btnBuscarEnfermedad.setBounds(346, 172, 132, 23);
+		btnBuscarEnfermedad.addActionListener(oyente);
+		btnBuscarEnfermedad.setBounds(346, 183, 132, 23);
 		panelIngreso.add(btnBuscarEnfermedad);
 		
+		btnIngresoSintomas = new JButton("Ingresar Sintomas");
+		btnIngresoSintomas.addActionListener(oyente);
+		btnIngresoSintomas.setBounds(10, 251, 165, 23);
+		panelIngreso.add(btnIngresoSintomas);
+		
 		btnBuscarMedicina = new JButton("Buscar Medicinas");
-		btnBuscarMedicina.setBounds(328, 324, 150, 23);
+		btnBuscarMedicina.addActionListener(oyente);
+		btnBuscarMedicina.setBounds(328, 333, 150, 23);
 		panelIngreso.add(btnBuscarMedicina);
 		
 		btnVolver = new JButton("Volver a Inicio");
+		btnVolver.addActionListener(oyente);
 		btnVolver.setBounds(311, 21, 171, 23);
 		panelIngreso.add(btnVolver);
 		
-		//private class milistener implements ActionListener
-		//{
-			
-			
-			/*
+		txtResultado = new JTextField();
+		txtResultado.setBounds(10, 308, 193, 39);
+		panelIngreso.add(txtResultado);
+		txtResultado.setColumns(10);
+		
+		JLabel lblResultados = new JLabel("Resultados:");
+		lblResultados.setBounds(10, 283, 83, 14);
+		panelIngreso.add(lblResultados);
+		
+		txtLaEnfermedad = new JTextField();
+		txtLaEnfermedad.setBounds(346, 48, 136, 20);
+		panelIngreso.add(txtLaEnfermedad);
+		txtLaEnfermedad.setColumns(10);
+		
+		textLaMedicina = new JTextField();
+		textLaMedicina.setBounds(346, 215, 130, 20);
+		panelIngreso.add(textLaMedicina);
+		textLaMedicina.setColumns(10);
+		
+		
+
+		
+		
+		private class milistener implements ActionListener
+		{
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				el_vuelo.setVuelo1(Integer.parseInt(txtHombre.getText()), Integer.parseInt(txtMujer.getText()), Integer.parseInt(txtNinnio.getText()));
-				el_vuelo.setVuelo2(Integer.parseInt(txtHombres2.getText()), Integer.parseInt(txtMujeres2.getText()), Integer.parseInt(txtNinnios2.getText()));
-				el_vuelo.setVuelo3(Integer.parseInt(txtHombres3.getText()), Integer.parseInt(txtMujeres3.getText()), Integer.parseInt(txtNinnios3.getText()));
-				lblCantdosv.setText(Integer.toString(el_vuelo.totalvuelos()));
-				lblAdvertencia.setText(el_vuelo.adv());
-				txtPlvlPromedioV1.setText(el_vuelo.porcentajeVuelo1()+el_vuelo.cantidaddemaspersonasvuelo1());
-				txtPlvlPromedioV2.setText(el_vuelo.porcentajeVuelo2()+el_vuelo.cantidaddemaspersonasvuelo2());
-				txtPlvlPromedioV3.setText(el_vuelo.porcentajeVuelo3()+el_vuelo.cantidaddemaspersonasvuelo3());
-			*/
-			//}
+				if (e.getSource() ==  btnBuscarEnfermedad)
+				{
+					if(txtLaEnfermedad.getText().equals(null))
+					{
+						JOptionPane.showMessageDialog(null,"Ingrese el nombre de una enfermedad porfavor.","Infor box: "+"Advertencia",JOptionPane.INFORMATION_MESSAGE);
+					}
+					else
+					{
+						catalogo.verEnfermedad(txtLaEnfermedad.getText());
+						txtBuscarEnfermedad.setText(catalogo.verEnfermedad(txtLaEnfermedad.getText()));
+					}
+				 
+				}
+				if (e.getSource() ==  btnIngresoSintomas)
+				{
+					txtResultado.setText(catalogo.buscarEnfermedad(dolorCabeza, dolorEstomago, vomito, diarrea, estornudo, tos, dolorGeneral, faltaEnergia));
+				}
+				if (e.getSource() ==  btnBuscarMedicina)
+				{
+					if(textLaMedicina.getText().equals(null))
+					{
+						JOptionPane.showMessageDialog(null,"Ingrese la medicina porfavor.","Infor box: "+"Advertencia",JOptionPane.INFORMATION_MESSAGE);
+					}
+					else
+					{
+						catalogo.VerMedicina(textLaMedicina.getText());
+						txtMedicamentos.setText(catalogo.VerMedicina(textLaMedicina.getText()));
+					}
+					
+				}
+				if (e.getSource() ==  btnVolver)
+				{
+					
+					//
+				}
+			}
+			
+			
+		}
 		
 
 	}
