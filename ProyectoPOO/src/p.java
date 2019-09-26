7df4f8ce4babf4b81b5a126d975484abd0f3a7f8
Julio Monzón <mon19226@uvg.edu.gt>
2019-09-25 19:48:25 -0600

    Un cambio no significativo

--- ProyectoPOO/src/p.java
+++ ProyectoPOO/src/p.java
@@ -3,6 +3,6 @@ public class p {
 	public static void main(String []args) {
 		CatalogoSalud c = new CatalogoSalud();
 		System.out.println(c.verEnfermedad("esta"));
-		System.out.println(c.BuscarMedicina("Pepto"));
+		System.out.println(c.VerMedicina("Pepto"));
 	}
 }
